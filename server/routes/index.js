9cb412d0e0dba57b955749bde4c4ee73699ee8e3
Blackstock <charlieblackstock@gmail.com>
2015-10-23 12:00:04 -0600

    added read quest

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,37 +11,70 @@ router.get('/', function(req, res, next) {
 //CREATE
 router.post('/api/v1/todos', function(req, res) {
 
-    var results = [];
+  var results = [];
 
-    // Grab data from http request
-    var data = {text: req.body.text, complete: false};
+  // Grab data from http request
+  var data = {text: req.body.text, complete: false};
 
-    // Get a Postgres client from the connection pool
-    pg.connect(connectionString, function(err, client, done) {
-        // Handle connection errors
-        if(err) {
-          done();
-          console.log(err);
-          return res.status(500).json({ success: false, data: err});
-        }
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, function(err, client, done) {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({ success: false, data: err});
+    }
 
-        // SQL Query > Insert Data
-        client.query("INSERT INTO items(text, complete) values($1, $2)", [data.text, data.complete]);
+    // SQL Query > Insert Data
+    client.query("INSERT INTO items(text, complete) values($1, $2)", [data.text, data.complete]);
 
-        // SQL Query > Select Data
-        var query = client.query("SELECT * FROM items ORDER BY id ASC");
+    // SQL Query > Select Data
+    var query = client.query("SELECT * FROM items ORDER BY id ASC");
 
-        // Stream results back one row at a time
-        query.on('row', function(row) {
-            results.push(row);
-        });
-
-        // After all data is returned, close connection and return results
-        query.on('end', function() {
-            done();
-            return res.json(results);
-        });
+    // Stream results back one row at a time
+    query.on('row', function(row) {
+      results.push(row);
     });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      done();
+      return res.json(results);
+    });
+  });
+
+});
+
+//READ
+router.get('/api/v1/todos', function(req, res) {
+
+  var results = [];
+
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, function(err, client, done) {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({ success: false, data: err});
+    }
+
+    // SQL Query > Select Data
+    var query = client.query("SELECT * FROM items ORDER BY id ASC;");
+
+    // Stream results back one row at a time
+    query.on('row', function(row) {
+      results.push(row);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      done();
+      return res.json(results);
+    });
+
+  });
+
 });
 
 module.exports = router;
